d4d2faafedb9c2618599a3da3faa22bbc78c9e5f
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-06-27 15:23:49 +1000

    Added more IRAP compliance checks

--- lib.php
+++ lib.php
@@ -5,13 +5,21 @@ function tool_password_check_password_policy($password){
     if (!(is_siteadmin())) {
         $errs = '';
 
-        //Check for fname, lname, city, username, email address, domain name of email
-        $email = $USER->email;
-        
+        //IRAP Certification checks
+        //no repeated characters X
+        //a single dictionary word
+        //no numeric sequences X
+        //no personal information X
+        //No repeat passwords
+
+        //NIST Recommendations - beyond above IRAP
+        //Black list of compromised passwords - HARD
+        //No service name in password
+
+        // Personal Information
+        //Check for fname, lname, city, username
         $badstrings = array($USER->firstname, $USER->lastname,
-                        $USER->city, $USER->username,
-                        );
-        $found = false;
+                        $USER->city, $USER->username);
         
         foreach ($badstrings as $string) {
             if (stripos($password, $string) !== false) {
@@ -19,7 +27,19 @@ function tool_password_check_password_policy($password){
             }
         }
 
-        //Check for significant dates
+        // Check for sequential digits.
+        $seqdigits = 3;
+        $digitpattern = '/\d{'.$seqdigits.',}/u';
+        if (preg_match($digitpattern, $password) === 1){
+            $errs .= "Password contains numeric sequence.\n";
+        }
+
+        // Check for repeated characters.
+        $characterpattern = '/(.)\1{1,}/';
+        if (preg_match($characterpattern, $password) === 1){
+            $errs .= "Password contains repeated characters.\n";
+        }
+
         return $errs;
     }
 }
