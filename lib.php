10c295b01c6d9c8cdb51932cc8b37cb4459d6b4b
[Peter Burnett] <[peterburnett@catalyst-au.net]>
2019-07-11 09:50:39 +1000

    Removed unnecessary require statements

--- lib.php
+++ lib.php
@@ -21,7 +21,7 @@
  * @copyright 2019 Peter Burnett <peterburnett@catalyst-au.net>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-require_once(__DIR__.'/../../../config.php');
+defined('MOODLE_INTERNAL') || die;
 
 /**
  * Validates the password provided against the password policy configured in the plugin admin
