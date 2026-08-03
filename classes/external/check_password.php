9ef76ad72d208af7458ca4203e1c079edc31bf2a
Cameron Ball <cameronball@catalyst-au.net>
2026-08-04 00:40:08 +0800

    Really fix external API compatibility

--- classes/external/check_password.php
+++ classes/external/check_password.php
@@ -26,6 +26,8 @@ namespace tool_passwordvalidator\external;
 
 defined('MOODLE_INTERNAL') || die();
 
+require_once($CFG->libdir . '/externallib.php');
+
 use external_api;
 use external_function_parameters;
 use external_single_structure;
--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024111406;
+$plugin->version   = 2024111407;
 $plugin->release   = $plugin->version;
 $plugin->requires  = 2016052300;
 $plugin->component = 'tool_passwordvalidator';
